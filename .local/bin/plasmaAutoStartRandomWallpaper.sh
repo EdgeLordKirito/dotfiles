ca311fdb7c6edf960457a36265b6c68f1759509e
EdgeLordKirito <Leiteritz.till@gmail.com>
2025-11-04 23:45:39 +0100

    general changes

--- .local/bin/plasmaAutoStartRandomWallpaper.sh
+++ .local/bin/plasmaAutoStartRandomWallpaper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Replace path with the path to your Wallpapers
-wallpapers="$HOME/Pictures/Wallpapers"
+wallpapers="$HOME/Pictures/wallpapers"
 $HOME/.local/bin/plasmaWallpaperSetterFromDir.sh "$wallpapers"
--- .zprofile
+++ .zprofile
@@ -1,2 +1,3 @@
+source ~/.zshrc
 #export PATH="$HOME/.local/bin:$PATH"
 #export XDG_HOME_CONFIG="$HOME/.config"
